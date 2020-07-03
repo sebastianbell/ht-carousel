73b351ded3b0545015f8f5e2df340c5ecae5e20f
Sebastian Bell <19672113+sebastianbell@users.noreply.github.com>
2020-07-03 12:06:01 -0700

    Checkpoint

--- src/components/Carousel.vue
+++ src/components/Carousel.vue
@@ -1,39 +1,44 @@
 <template>
   <section class="carousel">
     <div class="main-image">
-      <img :src="mainImage" />
+      <img :src="mainImageUrl" />
     </div>
     <div class="control-strip">
-      <img v-for="url in imageUrls" :src="url" :key="url" />
+      <img
+        v-for="url in imageUrls"
+        :src="url"
+        :key="url"
+        v-on:click="updateImage"
+      />
     </div>
   </section>
 </template>
 
 <script>
+const imageUrls = [
+  "https://upload.wikimedia.org/wikipedia/commons/6/6e/A_Song_Bird_%28177187493%29.jpeg",
+  "https://upload.wikimedia.org/wikipedia/commons/4/4a/19-06-170-carter.jpg",
+  "https://upload.wikimedia.org/wikipedia/commons/d/da/Dog-shaking-itself.jpg",
+  "https://upload.wikimedia.org/wikipedia/commons/d/de/Chicken_from_Venezuela.jpg",
+  "https://upload.wikimedia.org/wikipedia/commons/4/47/Raccoon_on_Log.jpg",
+  "https://upload.wikimedia.org/wikipedia/commons/9/95/Testudohermanni.jpg",
+  "https://upload.wikimedia.org/wikipedia/commons/8/8d/Squirrel_holding_nut.jpg",
+];
+
 export default {
   name: "Carousel",
   data() {
-    const imageUrls = [
-      "https://upload.wikimedia.org/wikipedia/commons/6/6e/A_Song_Bird_%28177187493%29.jpeg",
-      "https://upload.wikimedia.org/wikipedia/commons/4/4a/19-06-170-carter.jpg",
-      "https://upload.wikimedia.org/wikipedia/commons/d/da/Dog-shaking-itself.jpg",
-      "https://upload.wikimedia.org/wikipedia/commons/d/de/Chicken_from_Venezuela.jpg",
-      "https://upload.wikimedia.org/wikipedia/commons/4/47/Raccoon_on_Log.jpg",
-      "https://upload.wikimedia.org/wikipedia/commons/9/95/Testudohermanni.jpg",
-      "https://upload.wikimedia.org/wikipedia/commons/8/8d/Squirrel_holding_nut.jpg",
-    ];
-
-    // const endpoint = "https://run.mocky.io/v3/6d1e82b6-26e3-4c12-951b-fadbf008cff4";
-
-    // fetch(endpoint)
-    //   .then((response) => response.json())
-    //   .then((data) => console.log(data));
-
-    let mainImage = imageUrls[0];
     return {
-      mainImage,
+      mainImageUrl: imageUrls[0],
       imageUrls,
     };
+  },
+  methods: {
+    updateImage: function(event) {
+      if (event) {
+        this.mainImageUrl = event.target.src;
+      }
+    },
   },
 };
 </script>

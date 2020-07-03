69aede34e8dd48a7ff42122e04542625651171e0
Sebastian Bell <19672113+sebastianbell@users.noreply.github.com>
2020-07-03 16:10:44 -0700

    Added arrows
    Known issues: there's a strange bug tracking the index that happens sometimes when switching between clicking on the strip and the arrows, working on that now.

--- src/components/Carousel.vue
+++ src/components/Carousel.vue
@@ -2,12 +2,15 @@
   <section class="carousel">
     <div class="main-image">
       <img :src="mainImageUrl" />
+      <div class="arrow prev" v-on:click="prevImage"></div>
+      <div class="arrow next" v-on:click="nextImage"></div>
     </div>
     <div class="control-strip">
       <img
-        v-for="url in imageUrls"
+        v-for="(url, index) in imageUrls"
         :src="url"
         :key="url"
+        :data-index="index"
         v-on:click="updateImage"
       />
     </div>
@@ -18,8 +21,8 @@
 // Image URLs endpoint.
 const endPointUrl =
   "https://run.mocky.io/v3/c5d6233d-84f9-40b7-9801-f4dbd398eceb";
-// The array of image URLs, intit with a placeholder.
-let imageUrls = [
+// Array of image URLs, intit with a placeholder or default images.
+const placeholderImageUrls = [
   "https://upload.wikimedia.org/wikipedia/commons/c/ce/Transparent.gif",
 ];
 
@@ -28,8 +31,10 @@ export default {
   data() {
     this.fetchImageUrls();
     return {
-      mainImageUrl: imageUrls[0],
-      imageUrls,
+      imageUrls: placeholderImageUrls,
+      imagesLength: placeholderImageUrls.length,
+      mainImageUrl: placeholderImageUrls[0],
+      imageIndex: 0,
     };
   },
   methods: {
@@ -39,15 +44,37 @@ export default {
         .get(endPointUrl)
         .then((result) => {
           this.imageUrls = result.data.images;
+          this.imagesLength = this.imageUrls.length;
           this.mainImageUrl = result.data.images[0];
         })
         .catch((error) => {
-          window.console.log(error.response);
+          window.console.error(error.response);
         });
     },
     updateImage: function(event) {
       if (event) {
         this.mainImageUrl = event.target.src;
+        this.imageIndex = event.target.getAttribute("data-index");
+        // console.log(this.imageIndex);
+        // console.log(event.target);
+      }
+    },
+    prevImage: function(event) {
+      if (event && this.imageIndex > 0) {
+        this.imageIndex -= 1;
+        this.mainImageUrl = this.imageUrls[this.imageIndex];
+        console.log(this.imageIndex);
+      } else {
+        // console.log("nope!");
+      }
+    },
+    nextImage: function(event) {
+      if (event && this.imagesLength - 1 > this.imageIndex) {
+        this.imageIndex += 1;
+        this.mainImageUrl = this.imageUrls[this.imageIndex];
+        console.log(this.imageIndex);
+      } else {
+        // console.log("nope!");
       }
     },
   },
@@ -64,10 +91,35 @@ export default {
   display: flex;
   height: 80vh;
   justify-content: center;
+  position: relative;
 }
 
 .main-image > img {
   max-height: 80vh;
+}
+
+.main-image > .arrow {
+  background-image: url(../assets/arrow.svg);
+  background-size: contain;
+  background-repeat: no-repeat;
+  cursor: pointer;
+  height: 100px;
+  opacity: 0.3;
+  position: absolute;
+  width: 100px;
+}
+
+.main-image > .arrow:hover {
+  opacity: 0.5;
+}
+
+.main-image > .arrow.prev {
+  left: 0;
+  transform: scale(-1, 1);
+}
+
+.main-image > .arrow.next {
+  right: 0;
 }
 
 .control-strip {

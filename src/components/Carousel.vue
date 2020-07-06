dbfe99229014b22c0fa55da7a5e4b4fa76e1751f
Sebastian Bell <19672113+sebastianbell@users.noreply.github.com>
2020-07-05 22:12:17 -0700

    Bugfix

--- src/components/Carousel.vue
+++ src/components/Carousel.vue
@@ -142,7 +142,7 @@ export default {
       const queryString = window.location.search;
       const urlParams = new URLSearchParams(queryString);
       const img = urlParams.get("img");
-      if (parseInt(img) && img >= 0 && img <= this.imagesLength + 1) {
+      if (parseInt(img) && img >= 0 && img <= this.imagesLength - 1) {
         this.mainImageUrl = this.imageUrls[img];
         this.imageIndex = img;
       }
